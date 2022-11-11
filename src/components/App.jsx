98979bbabab258005b6982f7716fca12003f6164
SuhakMV <sugamish@gmail.com>
2022-11-11 17:53:10 +0200

    11111753

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,44 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import fetchGallery from "api/galleryApi";
+
+import Searchbar from "./Searchbar/Searchbar";
+
+
+export default class App extends Component {
+  state = {
+    pictures: [],
+    query: '',
+
+  }
+
+  async searchImages() {
+    const { query } = this.state;
+    try {
+      const data = await fetchGallery(query);
+      this.setState({
+        pictures: data.hits,
+      });
+      //console.log(data, '--res');
+    } catch (error) {
+
+    }
+    
+  }
+
+  getPictures = () => {
+
+  }
+
+  async componentDidMount() {
+    //this.getPictures();
+    fetchGallery('react').then(console.log);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
 };
